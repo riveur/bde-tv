b31a4b4411acdb551ff065de7361c8bcce58cb6c
riveur <riveur.cpy@gmail.com>
2025-01-14 05:43:06 +0100

    feat: error pages

--- inertia/pages/errors/not_found.tsx
+++ inertia/pages/errors/not_found.tsx
@@ -1,11 +1,13 @@
+import { Head } from '@inertiajs/react'
+
 export default function NotFound() {
   return (
     <>
-      <div className="container">
-        <div className="title">Page not found</div>
-
-        <span>This page does not exist.</span>
-      </div>
+      <Head title="Page introuvable" />
+      <main className="min-h-screen flex items-center justify-center flex-col gap-2">
+        <h1 className="text-4xl font-bold">404</h1>
+        <p>Page introuvable</p>
+      </main>
     </>
   )
 }
--- inertia/pages/errors/server_error.tsx
+++ inertia/pages/errors/server_error.tsx
@@ -1,11 +1,14 @@
+import { Head } from '@inertiajs/react'
+
 export default function ServerError(props: { error: any }) {
   return (
     <>
-      <div className="container">
-        <div className="title">Server Error</div>
-
-        <span>{props.error.message}</span>
-      </div>
+      <Head title="Erreur interne" />
+      <main className="min-h-screen flex items-center justify-center flex-col gap-2">
+        <h1 className="text-4xl font-bold">{props.error.status}</h1>
+        <p>Erreur interne</p>
+        <p className="text-muted">{props.error.message || "L'erreur est inconnue"}</p>
+      </main>
     </>
   )
 }
