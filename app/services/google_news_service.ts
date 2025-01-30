a053775f64124e7eaaa55dad0c7ebca03017352d
riveur <riveur.cpy@gmail.com>
2025-01-30 18:32:20 +0100

    refactor(google_news): improve cache handling

--- app/services/google_news_service.ts
+++ app/services/google_news_service.ts
@@ -9,39 +9,43 @@ const API_KEY = env.get('SERPAPI_API_KEY')
 
 export class GoogleNewsService {
   async getArticles(topicToken: string) {
+    try {
+      const articles = await cache.getOrSet({
+        key: `google-news:${topicToken}`,
+        factory: () => this.fetchArticles(topicToken),
+        ttl: '8h',
+      })
+
+      return articles
+    } catch (error: unknown) {
+      return null
+    }
+  }
+
+  private async fetchArticles(topicToken: string) {
     if (!API_KEY || !topicToken) {
       return null
     }
 
-    try {
-      return cache.getOrSet({
-        key: `google-news:${topicToken}`,
-        factory: async () => {
-          const response = await ky.get('https://serpapi.com/search.json', {
-            searchParams: {
-              engine: 'google_news',
-              gl: 'FR',
-              hl: 'fr',
-              topic_token: topicToken,
-              api_key: API_KEY,
-            },
-          })
+    const response = await ky.get('https://serpapi.com/search.json', {
+      searchParams: {
+        engine: 'google_news',
+        gl: 'FR',
+        hl: 'fr',
+        topic_token: topicToken,
+        api_key: API_KEY,
+      },
+    })
 
-          const data = await response.json()
-          const articles = await googleNewsArticlesValidator.validate(data)
+    const data = await response.json()
+    const articles = await googleNewsArticlesValidator.validate(data)
 
-          return {
-            title: articles.title,
-            articles: articles.news_results.map((article) => ({
-              ...article,
-              date: this.parseDateToISO(article.date),
-            })),
-          }
-        },
-        ttl: '8h',
-      })
-    } catch (error: unknown) {
-      return null
+    return {
+      title: articles.title,
+      articles: articles.news_results.map((article) => ({
+        ...article,
+        date: this.parseDateToISO(article.date),
+      })),
     }
   }
 
